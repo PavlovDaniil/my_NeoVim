c4df4d30f89a043c8b50a11b86cb3eb1b6403f98
Pavlov Daniil <pavlovdaniil492@gmail.com>
2025-11-19 01:58:37 +0300

    добавлено описанме ошибок
    я добавил описание ошибок и теперь если будет ошибка то справа будет
    примерное описание что за ошибка

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -3,4 +3,17 @@ vim.o.relativenumber = true
 vim.cmd("syntax on")
 vim.cmd("filetype plugin indent on")
 vim.o.termguicolors=true
-vim.o.tabstop = 4 
+vim.o.tabstop = 4
+
+-- буфер обмена
+vim.opt.clipboard = "unnamedplus"
+
+-- виртуальные отступы
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "●",  -- любой символ или пусто
+    spacing = 2,   -- отступ от текста
+  },
+  signs = true,     -- значки слева (как сейчас)
+  underline = true, -- подчёркивание ошибки
+})
