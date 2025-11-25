10ea16f60d63b68c31826f66b0e1f7a5d1ec7751
Pavlov Daniil <pavlovdaniil492@gmail.com>
2025-11-26 00:44:52 +0300

    добавлен плагин telescope
    добавлен плагин telescope для быстрой навигации по проектам и добавлены
    keymaps для него

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,14 @@
+vim.g.mapleader = " "
+local map = vim.keymap.set
+
 -- Открыть / закрыть дерево файлов
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+map("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 -- Быстро открыть терминал
-vim.keymap.set("n", "<leader>n", ":ToggleTerm<CR>", { noremap = true, silent = true })
+map("n", "<leader>n", ":ToggleTerm<CR>", { noremap = true, silent = true })
 
+-- Telescop
+map("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true })
+map("n", "<leader>fg", ":Telescope live_grep<CR>", { noremap = true, silent = true })
+map("n", "<leader>fb", ":Telescope buffers<CR>", { noremap = true, silent = true })
+map("n", "<leader>fh", ":Telescope help_tags<CR>", { noremap = true, silent = true })
