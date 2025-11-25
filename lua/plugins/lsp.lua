5d7951b58238c10adf4f3c60f991ce73da7c6fb8
Pavlov Daniil <pavlovdaniil492@gmail.com>
2025-11-25 13:24:50 +0300

    исправление бага и добавление .gitignore
    пришлось убрать настройку для python и добавил .gitignor куда вписал
    свой файл чтобы тестить работу neovim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,16 +1,3 @@
 return {
   "neovim/nvim-lspconfig",
-  function()
-    local lspconfig = require("lspconfig")
-    lspconfig.pyright.setup({
-	settins = {
-	  pyright = {
-	    python = {
-		pythonPath = ".venv/bin/python"
-		}
-	  }
-	}
-    })
-  end,
-
 }
