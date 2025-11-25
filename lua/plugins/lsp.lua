df6a222f4a5a5a1889a7a7ba32edbb845e591d1d
Pavlov Daniil <pavlovdaniil492@gmail.com>
2025-11-25 11:48:13 +0300

    улучшена работа с python UV
    в плагине esp улучшена работа с uv при помощи настройки

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,16 @@
 return {
   "neovim/nvim-lspconfig",
+  function()
+    local lspconfig = require("lspconfig")
+    lspconfig.pyright.setup({
+	settins = {
+	  pyright = {
+	    python = {
+		pythonPath = ".venv/bin/python"
+		}
+	  }
+	}
+    })
+  end,
+
 }
